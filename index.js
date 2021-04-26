d39d2a915230f2f7a5abe09d075752d616d3c25e
Pawel Zubkiewicz <pzubkiewicz@ptc.com>
2021-04-26 18:32:41 +0200

    fix: throw Error when logger not initialized

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const isTest = process.env.NODE_ENV === 'test' && process.env.DEBUG !== 'ON'
 
-module.exports = (file) => (...args) => {
+module.exports = (file) => {
   const fileName = file.split('/').slice(-1)[0];
   if(!fileName.includes('.js')) {
     throw new Error('Logger not initialized. Add `(__filename)` to the import line.')
   }
+  return logger(fileName)
+}
+
+const logger = (fileName) => (...args) => {
   if (!isTest) {
     console.log(`[${fileName}]`, ...args)
   }
