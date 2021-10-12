65cfd970f03abe08bbe469cd44f821d0f34bf5f5
Pawel Zubkiewicz <pzubkiewicz@ptc.com>
2021-10-12 18:59:12 +0200

    feat: add suport for jsx and ts files

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const isTest = process.env.NODE_ENV === 'test' && process.env.DEBUG !== 'ON'
 
 module.exports = (file) => {
   const fileName = file.split('/').slice(-1)[0];
-  if(!fileName.includes('.js')) {
-    throw new Error('Logger not initialized. Add `(__filename)` to the import line.')
+  const extensions = [".js", ".jsx", ".ts"];
+  if(extensions.some(ext => fileName.includes(ext))) {
+    return logger(fileName)
   }
-  return logger(fileName)
+  throw new Error('Logger not initialized. Add `(__filename)` to the import line.')
 }
 
 const logger = (fileName) => (...args) => {
